97e04fc43ed7d6469a89320e48235e2cae443e2a
Vishal Panghal <vishalsinghpanghal@gmail.com>
2026-05-23 14:46:57 +0530

    taliwind + daisyUI + NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import NavBar from "./NavBar"
 
 function App() {
 
-
   return (
     <>
-    <h1>Hello World</h1>
+    <NavBar />
+      <div data-theme="abyss" className="bg-base-200 h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Hello world </h1>
+      </div>
     </>
   )
 }
